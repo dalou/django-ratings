43f3d31326f8889337594abbd0bd61ca284853d6
dalou <autrusseau.damien@gmail.com>
2016-01-18 13:32:40 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ tests_require = [
 
 setup(
     name='django-ratings',
-    version=".".join(map(str, __import__('djangoratings').__version__)),
+    version=".".join(map(str, (0, 3, 7))),
     author='David Cramer',
     author_email='dcramer@gmail.com',
     description='Generic Ratings in Django',
